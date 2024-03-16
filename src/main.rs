67b0fefbd6882a78c728b6ac1f4cdb7e77f6d2c7
naokie <naoki.endoh@gmail.com>
2024-03-17 03:03:33 +0900

    Rename package to actix-gcd and implement basic HTTP server with GCD form

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,26 @@
-use std::str::FromStr;
-use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer };
 
 fn main() {
-    let mut numbers = Vec::new();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+    });
 
-    for arg in env::args().skip(1) {
-        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
-    }
-
-    if numbers.len() == 0 {
-        eprintln!("Usage: gcd NUMBER ...");
-        std::process::exit(1);
-    }
-
-    let mut d = numbers[0];
-    for m in &numbers[1..] {
-        d = gcd(d, *m);
-    }
-
-    println!("The greatest common divisor of {:?} is {}", numbers, d);
+    println!("Serving on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000").expect("error binding server to address")
+        .run();
 }
 
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
-}
-
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
-
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,  3 * 7 * 11 * 13 * 19), 3 * 11);
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="post">
+            <input type="text" name="n"/>
+            <input type="text" name="m"/>
+            <button type="submit">Compute GCD</button>
+        </form>
+        "#,
+    )
 }
