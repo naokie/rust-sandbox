ab3965691efe6a0a3a136afd267e211c403ab7a1
naokie <naoki.endoh@gmail.com>
2024-03-19 03:03:33 +0900

    Rename package to mandelbrot and refactor code for Mandelbrot set calculations

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,36 @@
-use serde::Deserialize;
-use actix_web::{web, App, HttpResponse, HttpServer };
+use num::Complex;
+use std::str::FromStr;
 
-#[derive(Deserialize)]
-struct GcdParameters {
-    n: u64,
-    m: u64,
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
 }
 
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None,
+            }
         }
-        m = m % n;
     }
-    n
 }
 
 #[test]
-fn test_gcd() {
-    assert_eq!(gcd(14, 15), 1);
-
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                   3 * 7 * 11 * 13 * 19),
-               3 * 11);
-}
-
-fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
-    if form.n == 0 || form.m == 0 {
-        return HttpResponse::BadRequest()
-            .content_type("text/html")
-            .body("Computing the GCD with zero is boring.");
-    }
-
-    let response = format!(
-        "The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
-        form.n,
-        form.m,
-        gcd(form.n, form.m)
-    );
-
-    HttpResponse::Ok().content_type("text/html").body(response)
-}
-
-fn main() {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(get_index))
-            .route("/gcd", web::post().to(post_gcd))
-    });
-
-    println!("Serving on http://localhost:3000...");
-    server
-        .bind("127.0.0.1:3000")
-        .expect("error binding server to address")
-        .run();
-}
-
-fn get_index() -> HttpResponse {
-    HttpResponse::Ok().content_type("text/html").body(
-        r#"
-        <title>GCD Calculator</title>
-        <form action="/gcd" method="post">
-            <input type="text" name="n"/>
-            <input type="text" name="m"/>
-            <button type="submit">Compute GCD</button>
-        </form>
-        "#,
-    )
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
